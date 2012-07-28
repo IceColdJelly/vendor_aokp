d6b41bc10555caf0abb142eb593e16f9b1a41bb7
LorD ClockaN <davor@losinj.com>
2012-07-28 15:11:48 +0200

    update fingerprint for endeavoru

--- products/endeavoru.mk
+++ products/endeavoru.mk
@@ -22,7 +22,7 @@ PRODUCT_MODEL := HTC One X
 PRODUCT_MANUFACTURER := HTC
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=endeavoru BUILD_FINGERPRINT="htc_europe/endeavoru/endeavoru:4.0.3/IML74K/62864.11:user/release-keys" PRIVATE_BUILD_DESC="1.29.401.11 CL62864 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=endeavoru BUILD_FINGERPRINT="htc_europe/endeavoru/endeavoru:4.0.4/IMM76D/70826.2:userdebug/test-keys" PRIVATE_BUILD_DESC="2.05.401.2 CL70826 test-keys"
 
 # Copy over prebuilt boot animation
 PRODUCT_COPY_FILES +=  \
